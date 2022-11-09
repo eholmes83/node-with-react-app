1c1bb47adb030ea343adb8419e6af6b9103ff501
Ed Holmes <eholmes83@yahoo.com>
2022-11-09 13:12:54 -0500

    WIP - attempting to fix proxy issues

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-// TODO - remove changeOrigin properties to prevent prod issues
-
 module.exports = (app) => {
-  app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000' }))
-  app.use('/auth/google', createProxyMiddleware({ target: 'http://localhost:5000' }))
+  app.use(createProxyMiddleware('/api', { target: 'http://localhost:5000' }))
+  app.use(createProxyMiddleware('/auth/google', { target: 'http://localhost:5000' }))
 }
+
+// app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000' }));
+// app.use('/auth/google', createProxyMiddleware({ target: 'http://localhost:5000' }))
