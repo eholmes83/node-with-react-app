2879ac22c4c52867269e28c5c932aaf88db59ca4
Ed Holmes <eholmes83@yahoo.com>
2022-11-09 11:50:38 -0500

    added todo

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
+// TODO - remove changeOrigin properties to prevent prod issues
+
 module.exports = (app) => {
   app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }))
   app.use('/auth/google', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }))
