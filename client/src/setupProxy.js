c5b259917ed3093ed0af2a7a9c3152bf6b8bfc33
Ed Holmes <eholmes83@yahoo.com>
2022-11-09 17:15:01 -0500

    removing unnecessary code

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,6 +4,3 @@ module.exports = (app) => {
   app.use(createProxyMiddleware('/api', { target: 'http://localhost:5000' }))
   app.use(createProxyMiddleware('/auth/google', { target: 'http://localhost:5000' }))
 }
-
-// app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000' }));
-// app.use('/auth/google', createProxyMiddleware({ target: 'http://localhost:5000' }))
--- index.js
+++ index.js
@@ -10,12 +10,6 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
-
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -29,6 +23,5 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
-const host = '0.0.0.0'
 
-app.listen(PORT, host, () => console.log("Server starting..."))
+app.listen(PORT, () => console.log("Server starting..."))
