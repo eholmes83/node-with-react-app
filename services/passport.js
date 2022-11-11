338c0b48c9d43e4b615c10bfd038a808c7bc844e
Ed Holmes <eholmes83@yahoo.com>
2022-11-11 17:49:06 -0500

    removed client_old directory and refactored passport file

--- services/passport.js
+++ services/passport.js
@@ -22,11 +22,16 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => { existingUser ? done(null, existingUser) : new User({ googleId: profile.id }).save()
-      .then((user) => done(null, user))
-      })
+}, async (accessToken, refreshToken, profile, done) => {
+   const existingUser = await User.findOne({ googleId: profile.id })
+
+   if (existingUser) {
+      // already have a record with the given profile ID
+      return done(null, existingUser)
+  }
+    // new user, make new record
+    const user = await new User({ googleId: profile.id }).save()
+    done(null, user)
   })
 )
 
